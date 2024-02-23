beed977b4638929e6e3505070d63019b49cb54db
Umesh Kumar Gupta <136313732+Umeshindiainternet@users.noreply.github.com>
2024-02-23 16:05:28 +0530

    Update contactController.js
    Update contactController Add utm_url, ip

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,19 +1,28 @@
 const Contact = require('../models/Contact');
 const nodemailer = require('nodemailer');
+const https = require('http');
+
 
 const getContacts = async () => {
     const data = await Contact.find();
     return { status: true, data };
 };
 
-const postContact = async ({ name, email, phone, company, message }) => {
+const postContact = async ({ name, email, phone, company, message, utm_url, ip }) => {
+
+    let country_name = await getCountryName(ip);
+
     const newContact = new Contact({
         name,
         email,
         phone,
         company,
-        message
+        message,
+        utm_url,
+        ip,
+        country_name
     });
+
     const saveUser = await newContact.save();
 
     // Create a SMTP transport object
@@ -34,6 +43,8 @@ const postContact = async ({ name, email, phone, company, message }) => {
         to: '"Gravity Infosolutions" <info@gravityinfosolutions.com>',
         // to: '"Gravity Infosolutions" <hitenkhatri14@gmail.com>',
 
+        //to: '"Gravity Infosolutions" <sandeep@indiainternets.com>',
+
         subject: 'New Lead Entered',
 
         text: `<div>
@@ -42,6 +53,9 @@ const postContact = async ({ name, email, phone, company, message }) => {
                 <p>Phone: ${phone}</p>
                 <p>Company: ${company}</p>
                 <p>Message: ${message}</p>
+                <p>Utm Url: ${utm_url}</p>
+                <p>IP: ${ip}</p>
+                <p>Country Name: ${country_name}</p>
             </div>`,
 
         html: `<div>
@@ -50,6 +64,9 @@ const postContact = async ({ name, email, phone, company, message }) => {
                 <p>Phone: ${phone}</p>
                 <p>Company: ${company}</p>
                 <p>Message: ${message}</p>
+                <p>Utm Url: ${utm_url}</p>
+                <p>IP: ${ip}</p>
+                <p>Country Name: ${country_name}</p>
             </div>`
     };
 
@@ -80,3 +97,31 @@ module.exports = {
     deleteContact
 };
 
+function getCountryName(ip) {
+    return new Promise((resolve, reject) => {
+        // Make a request to the ipinfo.io API
+        //const url = '`https://ipinfo.io/${ip}/json`';
+        const url = `http://ip-api.com/json/${ip}`;
+
+        https.get(url, (response) => {
+            let data = '';
+
+            // A chunk of data has been received.
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received.
+            response.on('end', () => {
+                try {
+                    const ipInfo = JSON.parse(data);
+                    resolve(ipInfo.country); // Resolve with the country name
+                } catch (error) {
+                    reject(error); // Reject with the error if JSON parsing fails
+                }
+            });
+        }).on('error', (error) => {
+            reject(error); // Reject with the error if the HTTP request fails
+        });
+    });
+}

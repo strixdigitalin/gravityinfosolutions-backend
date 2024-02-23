417762944b9859fdeece15affca31249b442686c
Umesh Kumar Gupta <136313732+Umeshindiainternet@users.noreply.github.com>
2024-02-23 16:20:42 +0530

    Update contactController.js
    Test Mail Issue

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -40,12 +40,13 @@ const postContact = async ({ name, email, phone, company, message, utm_url, ip }
         // from: 'Gravity Infosolutions <gravityinfosolutions201@gmail.com>',
         from: 'Gravity Infosolutions <info@gravityinfosolutions.com',
 
-        to: '"Gravity Infosolutions" <info@gravityinfosolutions.com>',
+        //to: '"Gravity Infosolutions" <info@gravityinfosolutions.com>',
         // to: '"Gravity Infosolutions" <hitenkhatri14@gmail.com>',
 
         //to: '"Gravity Infosolutions" <sandeep@indiainternets.com>',
 
-        cc: '"CC Recipient" <umesh@indiainternets.com>',
+        //cc: '"CC Recipient" <umesh@indiainternets.com>',
+        to: '"Gravity Infosolutions" <umesh@indiainternets.com>',
 
         subject: 'New Lead Entered',
 

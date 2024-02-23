4bc502380ac13293be4d6aad6243d6cb0ff99978
Umesh Kumar Gupta <136313732+Umeshindiainternet@users.noreply.github.com>
2024-02-23 16:28:42 +0530

    Update contactRouter.js

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -1,29 +1,29 @@
-const express=require('express');
+const express = require('express');
 const { getContacts, postContact, deleteContact } = require('../controllers/contactController');
 const auth = require('../middleware/auth');
-const router=express.Router();
+const router = express.Router();
 
-router.get('/getContacts', async (req,res)=>{
-    const data=await getContacts();
+router.get('/getContacts', async (req, res) => {
+    const data = await getContacts();
     res.json(data);
 });
 
-router.post('/postContact', async (req,res)=>{
-    const data=await postContact({...req.body});
-    if(!data.status)
-    {
+router.post('/postContact', async (req, res) => {
+    req.body.ip = req.ip;
+    return req.ip;
+    const data = await postContact({ ...req.body });
+    if (!data.status) {
         return res.status(400).json(data);
     }
     res.json(data);
 });
 
-router.delete('/deleteContact/:id', async (req,res)=>{
-    const data=await deleteContact({id: req.params.id, auth: req.user});
-    if(!data.status)
-    {
+router.delete('/deleteContact/:id', async (req, res) => {
+    const data = await deleteContact({ id: req.params.id, auth: req.user });
+    if (!data.status) {
         return res.status(400).json(data);
     }
     res.json(data);
 });
 
-module.exports=router;
+module.exports = router;

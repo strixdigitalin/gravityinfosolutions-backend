ae5d02f77ff407432a59f5bc037ce0d9b9354303
Umesh Kumar Gupta <136313732+Umeshindiainternet@users.noreply.github.com>
2024-02-23 16:06:18 +0530

    Update Contact.js
    Update Model add utm_url, ip

--- models/Contact.js
+++ models/Contact.js
@@ -1,17 +1,20 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const mySchema=mongoose.Schema({
+const mySchema = mongoose.Schema({
     name: String,
-    email:String,
-    phone:String,
-    company:String,
-    message:String,
-    ts:{
+    email: String,
+    phone: String,
+    company: String,
+    message: String,
+    utm_url: String,
+    ip: String,
+    country_name: String,
+    ts: {
         type: String,
         default: new Date().getTime()
     }
 });
 
-const Contact=mongoose.model('Contact', mySchema);
+const Contact = mongoose.model('Contact', mySchema);
 
-module.exports=Contact;
+module.exports = Contact;
